a534f8da68bf948802d7ec8e6588b17df5e98355
rvverdug <rvverdug@gmail.com>
2024-03-29 10:28:39 +0100

    minor changes

--- gridsearch.sh
+++ gridsearch.sh
@@ -8,9 +8,12 @@
 #SBATCH --partition=gpu
 #SBATCH --gpu-bind=single:1
 source $HOME/.bashrc
+
 spack load miniconda3@4.10.3
 conda activate emergab 
 
-srun python $HOME/emergent-abstractions/ -m egg.nest.nest_local --game train --sweep grid_search/parameters.json --n_workers=1 --root_dir "grid_search/28-03b" --name "emergent-abstractions"
+cd $HOME/emergent-abstractions/
+
+srun python -m egg.nest.nest_local --game train --sweep grid_search/parameters.json --n_workers=25 --root_dir "grid_search/29-03-2" --name "emergent-abstractions"
 #just make sure system got time to save all the models and stuff...
 srun sleep 10
