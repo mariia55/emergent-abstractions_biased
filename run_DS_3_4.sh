323cd696ce2d8db2c967d361659d10522e081f94
rvverdug <rvverdug@gmail.com>
2024-04-09 20:57:28 +0000

    fixed errors

--- run_DS_3_4.sh
+++ run_DS_3_4.sh
@@ -18,10 +18,10 @@ spack load miniconda3
 conda activate eggfix
 
 # Navigate to the directory
-cd "$HOME/emergent-abstractions"
+cd "$HOME/emergent-abstractions/"
 
 # Run the Python script using srun with the absolute path to nest_local.py
-srun /home/student/r/rverdugo/miniconda3/envs/eggfix/bin/python train.py --batch_size 64 --n_epochs 300 --dimensions 4 4 4 --learning_rate 0.0005 --game_size 10 --hidden_size 128 --temp_update 0.99 --temperature 2 --save True --num_of_runs 5 --path "$HOME/emergent-abstractions/results/vague_ds_results
+srun /home/student/r/rverdugo/miniconda3/envs/eggfix/bin/python train.py --batch_size 64 --n_epochs 300 --dimensions 4 4 4 --learning_rate 0.0005 --game_size 10 --hidden_size 128 --temp_update 0.99 --temperature 2 --save True --num_of_runs 5 --path "$HOME/emergent-abstractions/results/vague_ds_results"
 
 # Make sure the system has time to save all the models and stuff
 srun sleep 10
