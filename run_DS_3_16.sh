8c368d6b0cc451b4711fc2e211118e4b9e7ca353
rvverdug <rvverdug@gmail.com>
2024-04-11 12:56:41 +0200

    minor change

--- run_DS_3_16.sh
+++ run_DS_3_16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J DS_3_16
-#SBATCH --time=48:00:00
+#SBATCH --time=8:00:00
 #SBATCH --mem=400gb
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
@@ -21,7 +21,7 @@ conda activate eggfix
 cd "$HOME/emergent-abstractions/"
 
 # Run the Python script using srun with the absolute path to nest_local.py
-srun /home/student/r/rverdugo/miniconda3/envs/eggfix/bin/python train.py --batch_size 64 --n_epochs 300 --dimensions 16 16 16 --learning_rate 0.0005 --game_size 10 --hidden_size 128 --temp_update 0.99 --temperature 2 --save True --num_of_runs 5 --path "$HOME/emergent-abstractions/results/vague_ds_results"
+srun /home/student/r/rverdugo/miniconda3/envs/eggfix/bin/python train.py --batch_size 64 --n_epochs 300 --dimensions 16 16 16 --learning_rate 0.0005 --game_size 10 --hidden_size 128 --temp_update 0.99 --temperature 2 --save True --num_of_runs 2 --path "$HOME/emergent-abstractions/results/additional_runs"
 
 # Make sure the system has time to save all the models and stuff
 srun sleep 10
